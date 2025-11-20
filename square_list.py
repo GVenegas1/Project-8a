8b9714f45b80e758c30ba171e0259dfbd8dd31e5
Gabriel Venegas <94145724+GVenegas1@users.noreply.github.com>
2025-11-19 21:02:12 -0800

    squared_list

--- square_list.py
+++ square_list.py
@@ -5,9 +5,15 @@
 #GitHub username: GVenegas1
 #Date: 11/19/2025
 #Description:
+"""This function takes a list of numbers and changes each number into its square.
+The original list is updated directly"""
 
 def square_list(numbers):
+
+    # Go through each number in the list along with its index
     for i in range(len(numbers)):
+
+        #Replace the numbers with its squared
         numbers[i] = numbers[i] ** 2
 
 #Example
