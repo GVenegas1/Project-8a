c58769a6d2cc34871ab65d108aa11ad7f39a0e16
Gabriel Venegas <94145724+GVenegas1@users.noreply.github.com>
2025-11-19 20:52:28 -0800

    squared_list

---
+++ square_list.py
@@ -0,0 +1,16 @@
+
+#project-8a
+
+#Gabriel Venegas
+#GitHub username: GVenegas1
+#Date: 11/19/2025
+#Description:
+
+def square_list(numbers):
+    for i in range(len(numbers)):
+        numbers[i] = numbers[i] ** 2
+
+#Example
+nums= [7,-3,12,9]
+square_list(nums)
+print(nums)
